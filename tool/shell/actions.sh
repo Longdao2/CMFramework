d1eeab9f188448e1901a4af9b10fcdb532b51a33
Longdao2 <daolong.v2@gmail.com>
2023-10-21 23:57:47 +0700

    [1.0.5] update shell script

---
+++ tool/shell/actions.sh
@@ -0,0 +1,289 @@
+#=================================================================================#
+# File         actions.sh                                                         #
+# Author       Long Dao                                                           #
+# About        https://louisvn.com                                                #
+# Version      1.0.5                                                              #
+# Release      10-30-2023                                                         #
+# Copyright    2023 (c) Belongs to Louisvn                                        #
+# Details      C/C++ project management tool - [SH] Actions                       #
+#=================================================================================#
+
+#---------------------------------------------------------------------------------#
+#                                    Ingen_ccov_reportcludes                                     #
+#---------------------------------------------------------------------------------#
+
+source $SHELL_DIR/apis.sh
+
+#---------------------------------------------------------------------------------#
+#                                   Definitions                                   #
+#---------------------------------------------------------------------------------#
+
+list_ccd="$CCFLAGS $CCOV_CC"
+list_ldd="$LDFLAGS $CCOV_LD"
+
+newproj_name="$2"
+newproj_dir="$BASE_DIR/$newproj_name"
+
+vsc_dir="$ROOT_DIR/.vscode"
+ccpp_file="$vsc_dir/c_cpp_properties.json"
+launch_file="$vsc_dir/launch.json"
+setting_file="$vsc_dir/settings.json"
+
+#---------------------------------------------------------------------------------#
+#                                     Process                                     #
+#---------------------------------------------------------------------------------#
+
+# =================================================================================
+# Action: clean
+# Detail: Clean all files in project output
+#
+if [ "$1" = "clean" ]; then
+  # Reset the check variables of the dependencies to their defaults
+  echo "check=3" > $DEPC_FILE
+
+  # Scan the list of existing output to be removed
+  list="$(ls -d $OUT_DIR -f $REPORT_HTML $DOC_DIR/$PROJ_RAW""_ccov.* 2>/dev/null)"
+
+  # If there is at least one output, the cleanup process will start
+  if ! [ "$list" = "" ]; then
+  process_start clean
+
+  # Print a message and sequentially remove the outputs
+  for path in $list; do
+      message_red "Removing $path" & rm -rf $path
+  done
+
+  process_end clean
+  fi
+
+# =================================================================================
+# Action: depend_init
+# Detail: Check for changes in the dependency list
+#
+elif [ "$1" = "depend_init" ]; then
+  if [ -f $CCD_FILE ]; then source $CCD_FILE; else CCD_DATA=""; fi
+  if [ -f $LDD_FILE ]; then source $LDD_FILE; else LDD_DATA=""; fi
+
+  check=0
+
+  if ! [ "$CCD_DATA" = "$list_ccd" ]; then rm -f $OUT_DIR/*.o & (( check += 1 )); fi
+  if ! [ "$LDD_DATA" = "$list_ldd" ]; then rm -f $PROJ_OBJ    & (( check += 2 )); fi
+
+  echo "check=$check" > $DEPC_FILE
+
+# =================================================================================
+# Action: depend_update
+# Detail: Update the dependency list if it was changed previously
+#
+elif [ "$1" = "depend_update" ]; then
+  if [ -f $DEPC_FILE ]; then source $DEPC_FILE; else check=3; fi
+
+  if [ $check = 1 ] || [ $check = 3 ]; then
+    echo "CCD_DATA='"$list_ccd"'" > $CCD_FILE
+  fi
+
+  if [ $check = 2 ] || [ $check = 3 ]; then
+    echo "LDD_DATA='"$list_ldd"'" > $LDD_FILE
+  fi
+
+  # Write to the log file information about the timestamp to begin a new build session
+  if [ -e $OUT_DIR ]; then
+    (echo; echo; date +'========================= '%H:%M:%S' '%Y-%m-%d' ========================='; echo) >> $LOG_FILE
+  fi
+
+# =================================================================================
+# Action: run
+# Detail: Run the executable already in the project
+#
+elif [ "$1" = "run" ]; then
+  process_start run
+  if [ -e $PROJ_EXE ]; then
+    check=0
+
+    # Record the start and end times of program execution
+    time_start=$(date +%s%N)
+    timeout --kill-after=0s $RUN_TIMEOUT $PROJ_EXE $VAR_ARGS && retval=$? || retval=$?
+    time_end=$(date +%s%N)
+
+    (( time_diff = (time_end - time_start) / 1000000 - 10 ))
+
+    if [ $retval = 124 ]; then
+      message_error "$RUN_TIMEOUT timeout has expired"
+    else
+      check=1
+    fi
+
+    if [ -e "$REPORT_RAW" ]; then
+      $ECHO "0.duration = $time_diff\n0.status = $check" >> $REPORT_RAW
+    fi
+
+  else
+    message_error "[$PROJ_EXE] does not exist"
+  fi
+  process_end run
+
+# =================================================================================
+# Action: report
+# Detail: Generate test report after running the program
+#
+elif [ "$1" = "report" ]; then
+  if ! [ "$(ls -f $OUT_DIR/*.{ret,gcno} 2>/dev/null)" = "" ]; then
+    process_start report
+
+    # Clean old report
+    list="$(ls -f $REPORT_HTML $DOC_DIR/$PROJ_RAW""_ccov.* 2>/dev/null)"
+    for path in $list; do
+      message_red "Removing $path"
+    done
+    rm -rf $list
+
+    # Generate report
+    # Test report available
+    if [ -e $REPORT_RAW ]; then
+      message_green "Generating test report to $REPORT_HTML"
+
+      if [ "$RUN_CCOV" = "on" ]; then
+        gen_test_report $(basename $CCOV_HTML)
+        gen_ccov_report
+      else
+        gen_test_report
+      fi
+      open_report "test" "$REPORT_HTML"
+
+    # Only CCOV report
+    elif [ "$RUN_CCOV" = "on" ]; then
+      gen_ccov_report
+      open_report "ccov" "$CCOV_HTML"
+    fi
+
+    process_end report
+  fi
+
+# =================================================================================
+# Action: move
+# Detail: Move to new project (automatically create if it doesn't exist)
+#
+elif [ "$1" = "move" ]; then
+  process_start move
+  if [ -e "$newproj_dir" ]; then
+    if [ -e "$newproj_dir/user_cfg.mk" ]; then
+      if [ "$PROJ_NAME" = "$newproj_name" ]; then
+        message_green "You are here!"
+      else
+        move_project "$PROJ_NAME" "$newproj_name"
+      fi
+    else
+      message_error "[$newproj_name] is a group of projects!"
+    fi
+
+  else
+    if user_response "The [$newproj_name] project does not exist. Do you want to create it?"; then
+      message_blue "Cloning $newproj_dir" & mkdir -p $newproj_dir & rm -rf $TEMP_DIR/out
+      cp -Rf $TEMP_DIR/* $newproj_dir
+      move_project "$PROJ_NAME" "$newproj_name"
+    fi
+  fi
+  process_end move
+
+# =================================================================================
+# Action: remove
+# Detail: Remove an existing project or group
+#
+elif [ "$1" = "remove" ]; then
+  process_start remove
+  if [ -e "$newproj_dir" ]; then
+    if [ "$newproj_name" = "$TEMP_NAME" ]; then
+      message_error "Cannot remove template [$TEMP_NAME] project"
+
+    elif user_response "Do you agree to remove this project or group ($newproj_name)?"; then
+      message_red "Removing $newproj_dir" & \
+      rm -r -f $newproj_dir/*; rmdir -p --ignore-fail-on-non-empty $newproj_dir
+      if [[ $PROJ_NAME =~ ^$newproj_name.*$ ]]; then
+        move_project "$PROJ_NAME" "$TEMP_NAME"
+      fi
+    fi
+
+  else
+    message_error "The [$newproj_name] project or group does not exist"
+  fi
+  process_end remove
+
+# =================================================================================
+# Action: import
+# Detail: Import a shared project or group from any CMFramework
+#
+elif [ "$1" = "import" ]; then
+  process_start import
+  zip_file="$3"
+
+  if [[ "$zip_file" == *.zip ]] && ls $zip_file 1>/dev/null 2>&1; then
+    if [ -e "$newproj_dir" ]; then
+      message_error "The [$newproj_dir] project or group already exist"
+    else
+      message_green "Importing into [$newproj_dir]" & \
+      mkdir -p $newproj_dir
+      unzip $zip_file -d $newproj_dir
+    fi
+
+  else
+    message_error "Zip file does not exist or incorrect format"
+  fi
+  process_end import
+
+# =================================================================================
+# Action: export
+# Detail: Pack your project or group ready to share
+#
+elif [ "$1" = "export" ]; then
+  process_start export
+  if [ -e "$newproj_dir" ]; then
+    for f in $(find $newproj_dir -type d -name "$(basename $OUT_DIR)"); do
+      message_red "Removing $f" & rm -rf $f
+    done
+
+    export_file=$SHARE_DIR/__${newproj_name//\//\~}.zip
+    message_green "Compressing to $export_file" & \
+    cd "$newproj_dir"; zip -r $export_file ./*
+
+  else
+    message_error "The [$newproj_dir] project or group does not exist"
+  fi
+  process_end export
+
+# =================================================================================
+# Action: vsinit
+# Detail: Configure VSCode so that the software links to the correct path
+#
+elif [ "$1" = "vsinit" ]; then
+  # Create vscode folder
+  mkdir -p $vsc_dir
+
+  # Copy all configuration files
+  cp -f $TOOL_DIR/extend/c_cpp.txt $ccpp_file
+  cp -f $TOOL_DIR/extend/launch.txt $launch_file
+  cp -f $TOOL_DIR/extend/settings.txt $setting_file
+
+  # Configuration for settings.json
+  sed -i "s|\[\[SED_TEMP_NAME\]\]|$TEMP_NAME|g" "$setting_file"
+
+  # Configuration for c_cpp_properties.json
+  buff=""
+  for item in $INC_DIRS; do
+    buff+='\n        "'$item'",'
+  done
+  sed -i "s|\[\[SED_INC_DIRS\]\]|$buff|g" "$ccpp_file"
+
+  # Configuration for launch.json
+  buff=""
+  eval "array=($VAR_ARGS)"
+  for item in "${array[@]}"; do
+    buff+='\n        "'"$(echo $item | sed 's|\\|\\\\|g; s|"|\\\\"|g; s|\||\\\||g')"'",'
+  done
+  sed -i "s|\[\[SED_PROJ_EXE\]\]|$PROJ_EXE|g; s|\[\[SED_VAR_ARGS\]\]|$buff|g; s|\[\[SED_STOP_ENTRY\]\]|$STOP_AT_ENTRY|g; s|\[\[SED_EXT_CONSOLE\]\]|$EXTERNAL_CONSOLE|g" "$launch_file"
+
+# =================================================================================
+fi
+
+#---------------------------------------------------------------------------------#
+#                                   End of file                                   #
+#---------------------------------------------------------------------------------#
--- tool/shell/apis.sh
+++ tool/shell/apis.sh
@@ -2,8 +2,8 @@
 # File         apis.sh                                                            #
 # Author       Long Dao                                                           #
 # About        https://louisvn.com                                                #
-# Version      1.0.3                                                              #
-# Update       10-04-2023                                                         #
+# Version      1.0.5                                                              #
+# Release      10-30-2023                                                         #
 # Copyright    2023 (c) Belongs to Louisvn                                        #
 # Details      C/C++ project management tool - [SH] Apis                          #
 #=================================================================================#
@@ -13,22 +13,16 @@
 #---------------------------------------------------------------------------------#
 
 # =================================================================================
-# Func   : process_start
-# Brief  : Used to print a message for initializing a process
+# GFunc  : process_start | process_end
+# Brief  : Used to print a message at the start or end of a process
 # Params : [1] name_process
 # Return : /None/
 #
-function process_start() {  #  
+function process_start() {
   $ECHO "$INVERT<<< Start $1 $RCOLOR\n"
 }
 
-# =================================================================================
-# Func   : process_end
-# Brief  : Used to print a message for the completion of a process
-# Params : [1] name_process
-# Return : /None/
-#
-function process_end() {  #  name_process
+function process_end() {
   $ECHO "$GRAY $INVERT"
   date +">>> Finish $1 at "%H:%M:%S" on "%Y-%m-%d" "
   $ECHO "$RCOLOR"
@@ -46,12 +40,12 @@ function message_green() { $ECHO "$GREEN>$RCOLOR $1";       }
 function message_blue () { $ECHO "$BLUE>$RCOLOR $1";        }
 
 # =================================================================================
-# Func   : gen_ccov
+# Func   : gen_ccov_report
 # Brief  : Used to generate a report for code coverage
 # Params : /None/
 # Return : /None/
 #
-function gen_ccov() {
+function gen_ccov_report() {
   message_green "Generating ccov report to $CCOV_HTML" & \
   $GCOVR_EXE --root $DEV_DIR --object-directory $OUT_DIR --html-details $CCOV_HTML || \
   message_error "CCOV report generation failed"
@@ -94,10 +88,42 @@ function user_response() {
 # Params : [1] old_project . [2] new_project
 # Return : /None/
 #
-function move_project() {  #  from | to
+function move_project() {
   message_blue "Moving [$1] -> [$2]" & \
   sed -i -e "s/^\s*PROJ_NAME\s*:*=.*$/  PROJ_NAME := ${2//\//\\/}/g" $ROOT_DIR/makefile & \
   $MAKE PROJ_NAME="$2" __forced=on vsinit
+}
+
+# =================================================================================
+# Func   : gen_test_report
+# Brief  : Generate a test report from test results (using utest)
+# Params : [1] ccov_name
+# Return : /None/
+#
+function gen_test_report() {
+  # Get data from the test result file
+  function inner_getvar() {  #  test_index | var_name
+    sed -n "s/^\s*$1.$2\s*=\s*//p" "$REPORT_RAW"
+  }
+
+  dos2unix -q $REPORT_RAW & \
+  cp -f $TOOL_DIR/extend/report.html $REPORT_HTML
+
+  sed -i "s|\[\[SED_FILE_NAME\]\]|$(basename $REPORT_HTML)|g; s|\[\[SED_REPORT_CCOV\]\]|$1|g; s|\[\[SED_PROJ_NAME\]\]|$(inner_getvar 0 proj_name)|g; s|\[\[SED_USER_NAME\]\]|$(inner_getvar 0 user_name)|g; s|\[\[SED_TIMESTAMP\]\]|$(inner_getvar 0 exe_time)|g; s|\[\[SED_TIME_EXEC\]\]|$(inner_getvar 0 duration)|g; s|\[\[SED_CHECK_STATUS\]\]|$(inner_getvar 0 status)|g" "$REPORT_HTML"
+
+  buff=""
+  all_test=$(inner_getvar 0 all_test)
+
+  for (( i=1; i<=all_test; i++ )); do
+    test_name=$(inner_getvar $i name)
+    test_brief=$(echo $(inner_getvar $i brief) | sed 's|\\|\\\\|g; s|"|\\"|g; s|\||\\\||g')
+    test_status=$(inner_getvar $i status)
+    test_duration=$(inner_getvar $i duration)
+    test_fail=$(inner_getvar $i fail)
+
+    buff+='\n    Add_Test("'$test_name'",'$test_brief','$test_duration',"'$test_status'","'$test_fail'");'
+  done
+  sed -i "s|\[\[SED_ADD_ALL_TEST\]\]|$buff|g" "$REPORT_HTML"
 }
 
 #---------------------------------------------------------------------------------#
